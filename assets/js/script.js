690f0ceafffc41e8042fcdbbf8deb75e60a9ead9
zayan <122380059+zayanzz@users.noreply.github.com>
2024-02-27 15:33:10 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function error_call_submitation_failed() {
+    alert('404 Error currently not available contact form please contact +97121892932')
+}
+
+setTimeout(function(){ 
+    document.getElementById("loading").classList.add("none");
+
+ }, 3000);
+
